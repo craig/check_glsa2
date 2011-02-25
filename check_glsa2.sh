d0af4a8678c667a6b1824d53bad4f941d305691b
craig <craig@haquarter.de>
2011-02-25 11:53:03 +0100

    unique names, return value handling (idl0r), fix whitelist handling, fix handling of glsa-check error

--- check_glsa2.sh
+++ check_glsa2.sh
@@ -9,44 +9,59 @@
 
 WHITELISTFILE=/etc/snmp/whitelist
 
-if [ -e "${WHITELISTFILE}" ]
+if [ -f "${WHITELISTFILE}" ]
 then
 	WHITELIST=($(cat ${WHITELISTFILE}))
 fi
 
 GLSALIST=($(glsa-check -t affected 2>/dev/null | egrep ^'[0-9]{6}-[0-9]{2}'))
-if [[ ${?} -ne 0 ]]
+if [[ ${?} -eq 0 ]]
 then
-	if [ ${#GLSALIST[@]} -eq 0 ]
-	then
-		echo "OK - system not affected by any GLSAs"
-		exit 0
-	fi
 
-	echo "ERROR - trouble running glsa-check to get list of GLSAs"
-	exit 3
-fi
-
-for ((i=0; i<${#GLSALIST[@]}; i++))
-do
-	if [ $(echo ${WHITELIST[@]} | grep ${GLSALIST[$i]} ) ]
-	then
-		continue
-	fi
-
-	NEW_PACKAGES=($(glsa-check -d ${GLSALIST[$i]} | awk -F: '/Affected package: / {print $2}' | tr '\n' ' '))
-	if [[ ${?} -ne 0 ]]
-	then
-		echo "ERROR - trouble running glsa-check to get package name for GLSA ${#GLSALIST[@]}"
-		exit 3
-	fi
-
-	for ((k=0; k<${#NEW_PACKAGES[@]}; k++))
+	for ((i=0; i<${#GLSALIST[@]}; i++))
 	do
-		GLSA_PACKAGES="${GLSA_PACKAGES} ${NEW_PACKAGES[$k]}"
+		# if this GLSA-nr is in my whitelist, try next
+		if [ "$(echo ${WHITELIST[@]} | grep ${GLSALIST[$i]} )" ]
+		then
+			continue
+		fi
+
+		# get affected packages for this GLSA
+		NEW_PACKAGES=($(glsa-check -d ${GLSALIST[$i]} 2>/dev/null | awk -F: '/Affected package: / {print $2}' | tr '\n' ' '))
+		if [[ ${?} -ne 0 ]]
+		then
+			echo "ERROR - trouble running glsa-check to get package name for GLSA ${#GLSALIST[@]}"
+			exit 3
+		fi
+
+		# only add NEW_PACKAGES[k] to the list of affected packages, if it's not in yet (thanks idl0r)
+		for ((k=0; k<${#NEW_PACKAGES[@]}; k++))
+		do
+			if [[ ! $(echo ${GLSA_PACKAGES[@]} | egrep "\<${NEW_PACKAGES[$k]}\>") ]]
+			then
+				GLSA_PACKAGES="${GLSA_PACKAGES} ${NEW_PACKAGES[$k]}"
+			fi
+		done
+
 	done
 
-done
+	# possibly, all vulnerable packages were whitelisted: check if affected packages is empty
+	if [ ${#GLSA_PACKAGES[@]} -eq 0 ]
+	then
+		echo "OK - system not affected by any GLSAs (${#WHITELIST[@]} whitelisted)"
+		exit 0
+	else
+		echo "CRITICAL - affecting GLSAs:${GLSA_PACKAGES}"
+		exit 2
+	fi
 
-echo "CRITICAL - affecting GLSAs:${GLSA_PACKAGES}"
-exit 2
+# glsa-check returned no vulnerable packages
+elif [[ ${?} -eq 1 ]]
+then
+	echo "OK - system not affected by any GLSAs (${#WHITELIST[@]} whitelisted)"
+	exit 0
+fi
+
+echo "ERROR - trouble running glsa-check to get list of GLSAs"
+exit 3
+
